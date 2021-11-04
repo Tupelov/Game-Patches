ba6f5f19054a45fb134a5b36c5d050d097faee72
Tupelov <64274155+Tupelov@users.noreply.github.com>
2021-11-04 16:35:30 -0400

    Add DigimonCSFileLog

---
+++ PC/DSCS/Reloaded/DigimonCSLog/DigimonCSLogHook/Program.cs
@@ -0,0 +1,63 @@
+﻿//using Reloaded.Hooks.ReloadedII.Interfaces;
+using Reloaded.Mod.Interfaces;
+using Reloaded.Mod.Interfaces.Internal;
+using System;
+using Reloaded.Hooks;
+using Reloaded.Hooks.Definitions;
+
+namespace DigimonCSLogHook
+{
+    public class Program : IMod
+    {
+        /// <summary>
+        /// Used for writing text to the console window.
+        /// </summary>
+        private ILogger _logger;
+
+        /// <summary>
+        /// Provides access to the mod loader API.
+        /// </summary>
+        private IModLoader _modLoader;
+
+        /// <summary>
+        /// An interface to Reloaded's the function hooks/detours library.
+        /// See: https://github.com/Reloaded-Project/Reloaded.Hooks
+        ///      for documentation and samples. 
+        /// </summary>
+        private IReloadedHooks _hooks;
+
+        private Mod _mod;
+
+        /// <summary>
+        /// Entry point for your mod.
+        /// </summary>
+        public void Start(IModLoaderV1 loader)
+        {
+            _modLoader = (IModLoader)loader;
+            _logger = (ILogger)_modLoader.GetLogger();
+            _modLoader.GetController<IReloadedHooks>().TryGetTarget(out _hooks);
+            
+            /* Your mod code starts here. */
+            _mod = new Mod(_logger, _hooks);
+        }
+
+        /* Mod loader actions. */
+        public void Suspend() { }
+        public void Resume() { }
+        public void Unload() { }
+
+        /*  If CanSuspend == false, suspend and resume button are disabled in Launcher and Suspend()/Resume() will never be called.
+            If CanUnload == false, unload button is disabled in Launcher and Unload() will never be called.
+        */
+        public bool CanUnload() => false;
+        public bool CanSuspend() => false;
+
+        /* Automatically called by the mod loader when the mod is about to be unloaded. */
+        public Action Disposing { get; }
+
+        /* This is a dummy for R2R (ReadyToRun) deployment.
+           For more details see: https://github.com/Reloaded-Project/Reloaded-II/blob/master/Docs/ReadyToRun.md
+        */
+        public static void Main() { }
+    }
+}
