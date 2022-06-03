6d67e39f2c118c2c0e8370eb93539a9eef89d4bb
Tupelov <64274155+Tupelov@users.noreply.github.com>
2022-06-03 18:57:53 -0400

    Added Disable Mouse Accelleration for JPOG

---
+++ PC/JPOG/Disable Mouse Accelleration/Configuration/Config.cs
@@ -0,0 +1,17 @@
+﻿using Disable_Mouse_Accelleration.Configuration.Implementation;
+using System.ComponentModel;
+
+namespace Disable_Mouse_Accelleration.Configuration
+{
+	public class Config : Configurable<Config>
+	{
+		/*
+            User Properties:
+                - Please put all of your configurable properties here.
+                - Tip: Consider using the various available attributes https://stackoverflow.com/a/15051390/11106111
+        
+            By default, configuration saves as "Config.json" in mod folder.    
+            Need more config files/classes? See Configuration.cs
+        */
+	}
+}
---
+++ PC/JPOG/Disable Mouse Accelleration/Mod.cs
@@ -0,0 +1,38 @@
+﻿using Reloaded.Hooks.ReloadedII.Interfaces;
+using Reloaded.Memory.Sources;
+using Reloaded.Mod.Interfaces;
+using System;
+using System.Diagnostics;
+using System.Runtime;
+
+namespace Disable_Mouse_Accelleration
+{
+	/// <summary>
+	/// Your mod logic goes here.
+	/// </summary>
+	public class Mod
+	{
+		private ILogger _logger;
+		private IReloadedHooks _hooks;
+		private Process _process;
+		public Mod(IReloadedHooks hooks, ILogger logger)
+		{
+			_logger = logger;
+			_hooks = hooks;
+			_process = Process.GetCurrentProcess();
+
+			foreach(ProcessModule module in _process.Modules)
+			{
+				if (module.ModuleName == "tguiinterface.dll")
+				{
+					Memory.Instance.SafeWrite(module.BaseAddress + 0x150C0, 0x000008C2);
+					logger.WriteLine("[Disable Mouse Accelleration]: Successfully Patched TGuiInterface.dll!");
+					continue;
+				}
+
+			}
+			
+			// TODO: Implement some mod logic    
+		}
+	}
+}
